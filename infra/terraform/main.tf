274367f7cb2ca74f4a5492833b6c2193cabefac0
Samy <samyboudaoud95@gmail.com>
2026-06-22 15:10:37 +0200

    feat: update Terraform configuration for Azure resources and add random string suffix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,27 +1,31 @@
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
 
-# Registre de conteneurs (images poussées par la CI)
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.prefix}acr"
+  name                = "${var.prefix}acr${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
 }
 
-# Cluster Kubernetes managé
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.prefix}-aks"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = "westeurope"
   dns_prefix          = var.prefix
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_B2s"
   }
 
@@ -30,38 +34,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# Autoriser l'AKS à tirer les images de l'ACR
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
 }
 
-# PostgreSQL managé
-resource "azurerm_postgresql_flexible_server" "pg" {
-  name                   = "${var.prefix}-pg"
-  resource_group_name    = azurerm_resource_group.rg.name
-  location               = azurerm_resource_group.rg.location
-  version                = "16"
-  administrator_login    = var.pg_admin_login
-  administrator_password = var.pg_admin_password
-  storage_mb             = 32768
-  sku_name               = "B_Standard_B1ms"
-  zone                   = "1"
-}
-
-# Coffre à secrets
-resource "azurerm_key_vault" "kv" {
-  name                = "${var.prefix}-kv"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-}
-
-# Stockage objet (upload de fichiers / preuves de défi)
 resource "azurerm_storage_account" "sa" {
-  name                     = "${var.prefix}storage"
+  name                     = "${var.prefix}st${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -72,6 +53,14 @@ resource "azurerm_storage_container" "uploads" {
   name                  = "uploads"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "${var.prefix}-kv-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 }
 
 data "azurerm_client_config" "current" {}
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,22 +1,11 @@
 variable "prefix" {
-  description = "Préfixe des ressources"
+  description = "Prefixe des ressources"
   type        = string
   default     = "ticketflow"
 }
 
 variable "location" {
-  description = "Région Azure"
+  description = "Region Azure"
   type        = string
   default     = "francecentral"
-}
-
-variable "pg_admin_login" {
-  type    = string
-  default = "ticketflowadmin"
-}
-
-variable "pg_admin_password" {
-  description = "Mot de passe admin PostgreSQL (à fournir via TF_VAR_pg_admin_password)"
-  type        = string
-  sensitive   = true
 }
